a98124b93af645f0a49f02a6a6a91b4e05ebec51
TopFirmino <furcas.lorenzo@gmail.com>
2025-09-26 01:46:23 +0200

    started buddy allocator implementation

--- tests/buddy_allocator.c
+++ tests/buddy_allocator.c
@@ -19,15 +19,26 @@ static inline uint8_t level_from_index(uint32_t i)
     return floor(log2(i));
 }
 
-static inline uint32_t first_node_index_from_level(uint8_t level)
+static inline uint32_t first_idx(uint8_t level)
 {
     return 1 << level;
 }
 
 static inline uint32_t node_offset_in_level(uint32_t index)
 {
-    return index - first_node_index_from_level(level_from_index(index));
+    return index - first_idx(level_from_index(index));
 }
+
+static inline uint32_t buddy_index(uint32_t index)
+{
+    return (index%2) * (index - 1) + (1 - (index%2)) * (index + 1);
+}
+
+static inline uint32_t parent_index(uint32_t index)
+{
+    return floor(index / 2);
+}
+
 
 int main()
 {
